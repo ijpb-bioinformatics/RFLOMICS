e0430f837de2d37f6c9a2c330df3f44604c76b5d
Nadia Bessoltane-Bentahar <nadia.bessoltane@inra.fr>
2022-07-05 02:23:17 +0200

    improve report

--- R/utils.R
+++ R/utils.R
@@ -496,7 +496,7 @@ MA.plot <- function(data, Adj.pvalue.cutoff, FC.cutoff, hypothesis=hypothesis, p
   #
   # return(p)
   Abundance <- logFC <- Adj.pvalue <- NULL
-  tmp <- select(data,"Abundance","logFC","Adj.pvalue") %>% rename(., baseMeanLog2=Abundance, log2FoldChange=logFC, padj=Adj.pvalue)
+  tmp <-dplyr::select(data,"Abundance","logFC","Adj.pvalue") %>% rename(., baseMeanLog2=Abundance, log2FoldChange=logFC, padj=Adj.pvalue)
   p <- ggpubr::ggmaplot(tmp, main = hypothesis,
                 fdr = Adj.pvalue.cutoff, fc = FC.cutoff, size = 0.4,
                 ylab = bquote(~Log[2] ~ "fold change"),
