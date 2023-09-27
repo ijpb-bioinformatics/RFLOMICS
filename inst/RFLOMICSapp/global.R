a1f47fe5d75f4f65a7e3e2863da0ca85414963b6
HULOT Audrey <audrey.hulot@inrae.fr>
2023-09-27 16:39:34 +0200

    cleaned omics.dic comment

--- inst/RFLOMICSapp/global.R
+++ inst/RFLOMICSapp/global.R
@@ -22,7 +22,7 @@ library(plotly)
 library(gridExtra)
 library(ggplot2)
 library(qgraph)
-library(colourpicker)  ## ADDED August 2022 button to select colors
+library(colourpicker)  
 library(circlize)
 library(RColorBrewer)
 library(grid)
@@ -60,16 +60,5 @@ source("06_module_annot_enrichment_clusterProf.R")
 source("07_module_data_integration_MOFA.R")
 source("08_module_data_integration_mixOmics.R") 
 source("coverPage.R")
-
-# #enableBookmarking(store = "server")
-# omics.dic <- list()
-# omics.dic[["RNAseq"]][["variableName"]] <- "genes"
-# omics.dic[["RNAseq"]][["valueType"]]    <- "counts"
-# 
-# omics.dic[["proteomics"]][["variableName"]] <- "proteins"
-# omics.dic[["proteomics"]][["valueType"]]    <- "XIC"
-# 
-# omics.dic[["metabolomics"]][["variableName"]] <- "metabolites"
-# omics.dic[["metabolomics"]][["valueType"]]    <- "XIC"
 
 
