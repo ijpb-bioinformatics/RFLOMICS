5701524f85ea71dab8015f5eefba51fcdae00615
HULOT Audrey <audrey.hulot@inrae.fr>
2023-11-28 10:27:19 +0100

    fixed problem with documentation (rflomicsUi wasn't a function)

--- R/ui.R
+++ R/ui.R
@@ -2,16 +2,21 @@
 #' @export
 #' @importFrom shinydashboard dashboardSidebar dashboardBody dashboardHeader dashboardPage
 #' @rawNamespace import(shiny, except = renderDataTable)
-rflomicsUI <- dashboardPage(
-  dashboardHeader(title = "RFLOMICS"),
-  dashboardSidebar(
-    
-    uiOutput("mysidebar")
-  ),
-  dashboardBody({
-    
-    uiOutput("mycontent")
-    
-  })
-) 
+rflomicsUI <- function(){
+   uiout <- dashboardPage(
+      dashboardHeader(title = "RFLOMICS"),
+      dashboardSidebar(
+        
+        uiOutput("mysidebar")
+      ),
+      dashboardBody({
+        
+        uiOutput("mycontent")
+        
+      })
+    )
+   return(uiout)
+  }
+
+rflomicsUI()
 
