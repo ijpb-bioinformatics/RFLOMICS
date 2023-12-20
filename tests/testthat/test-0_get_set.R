d9a295559f212192b53e6983c3039125050ed56e
HULOT Audrey <audrey.hulot@inrae.fr>
2023-12-20 13:51:04 +0100

    replaced with generateExample, tests passed

--- tests/testthat/test-0_get_set.R
+++ tests/testthat/test-0_get_set.R
@@ -2,47 +2,8 @@ library(testthat)
 library(RFLOMICS)
 
 # ---- Construction MAE RFLOMICS ready for differential analysis : ----
-ExpDesign <- RFLOMICS::read_exp_design(file = paste0(system.file(package = "RFLOMICS"), "/ExamplesFiles/ecoseed/condition.txt"))
-factorRef <- data.frame(factorName  = c("Repeat", "temperature" , "imbibition"),
-                        factorRef   = c("rep1",   "Low",          "DS"),
-                        factorType  = c("batch",  "Bio",          "Bio"),
-                        factorLevels= c("rep1,rep2,rep3", "Low,Medium,Elevated", "DS,EI,LI"))
 
-omicsData <- list(
-  RFLOMICS::read_omics_data(file = paste0(system.file(package = "RFLOMICS"), "/ExamplesFiles/ecoseed/transcriptome_ecoseed.txt")),
-  RFLOMICS::read_omics_data(file = paste0(system.file(package = "RFLOMICS"), "/ExamplesFiles/ecoseed/metabolome_ecoseed.txt")), 
-  RFLOMICS::read_omics_data(file = paste0(system.file(package = "RFLOMICS"), "/ExamplesFiles/ecoseed/proteome_ecoseed.txt")))
-
-MAE <- RFLOMICS::FlomicsMultiAssay.constructor(projectName = "Tests", 
-                                               omicsData   = omicsData,
-                                               omicsNames  = c("RNAtest", "metatest", "protetest"),
-                                               omicsTypes  = c("RNAseq","metabolomics","proteomics"),
-                                               ExpDesign   = ExpDesign,
-                                               factorRef   = factorRef)
-names(MAE) <- c("RNAtest", "metatest", "protetest")
-
-formulae <- RFLOMICS::GetModelFormulae(MAE = MAE) 
-
-contrastList <- RFLOMICS::getExpressionContrast(object = MAE, modelFormula = formulae[[1]]) |> purrr::reduce(rbind) |>
-  dplyr::filter(contrast %in% c("(temperatureElevated_imbibitionDS - temperatureLow_imbibitionDS)",
-                                "((temperatureLow_imbibitionEI - temperatureLow_imbibitionDS) + (temperatureMedium_imbibitionEI - temperatureMedium_imbibitionDS) + (temperatureElevated_imbibitionEI - temperatureElevated_imbibitionDS))/3",
-                                "((temperatureElevated_imbibitionEI - temperatureLow_imbibitionEI) - (temperatureElevated_imbibitionDS - temperatureLow_imbibitionDS))" ))
-
-
-MAE2 <- MAE
-
-MAE <- MAE |>
-  TransformData(     SE.name = "metatest",  transformMethod = "log2")          |>
-  RunNormalization(  SE.name = "metatest",  NormMethod = "totalSum")            |>
-  RunNormalization(  SE.name = "RNAtest",   NormMethod = "TMM")                 |>
-  RunNormalization(  SE.name = "protetest", NormMethod = "median")              |>
-  FilterLowAbundance(SE.name = "RNAtest")                                       |>
-  RunDiffAnalysis(   SE.name = "metatest",  DiffAnalysisMethod = "limmalmFit", contrastList = contrastList, modelFormula = formulae[[1]])  |>
-  RunDiffAnalysis(   SE.name = "protetest", DiffAnalysisMethod = "limmalmFit", contrastList = contrastList, modelFormula = formulae[[1]])  |>
-  RunDiffAnalysis(   SE.name = "RNAtest",   DiffAnalysisMethod = "edgeRglmfit", contrastList = contrastList, modelFormula = formulae[[1]]) |>
-  FilterDiffAnalysis(SE.name = "RNAtest",   Adj.pvalue.cutoff = 0.05, logFC.cutoff = 1.5) |>
-  integrationWrapper(omicsToIntegrate = c("protetest", "metatest"), silent = TRUE, cmd = FALSE) |>
-  integrationWrapper(omicsToIntegrate = c("RNAtest", "metatest"),   silent = TRUE, cmd = FALSE, method = "mixOmics")
+MAE <- generateExample(annotation = FALSE, coexp = FALSE, integration = FALSE)
 
 # ----- TESTS -----
 
@@ -54,8 +15,6 @@ test_that("Design accessors", {
 # ---- factor types ----
 
 test_that("Factors types", {
-  
-  expect_error(getFactorTypes(MAE[["RNAtest"]]))
   
   expect_identical(bioFactors(MAE), c("temperature", "imbibition"))
   expect_identical(batchFactors(MAE), c("Repeat"))
@@ -102,7 +61,6 @@ test_that("Operation DE lists", {
   
   
 })
-
 
 
 
