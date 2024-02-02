467865a8fd06033b2543d21351d765148a61e694
HULOT Audrey <audrey.hulot@inrae.fr>
2024-02-02 14:32:15 +0100

    added utils enrichment and exported new methods/functions

---
+++ R/utils-06_annot_enrichment.R
@@ -0,0 +1,109 @@
+######## INTERNAL - ANNOTATION CLUSTERPROFILER #########
+
+#' @title see_pathview
+#' @param ... Possible arguments for pathview function.
+#' @return nothing. Plot on the currently opened device.
+#' @keywords internal
+#' @importFrom grid grid.raster
+#' @importFrom stringr str_split
+#' @importFrom pathview pathview
+#' @importFrom png readPNG
+#' @noRd
+#'
+# Code from: https://stackoverflow.com/questions/60141841/how-to-get-pathview-plot-displayed-directly-rather-than-saving-as-a-file-in-r
+# It deletes every file created by pathview
+.see_pathview <- function(...) {
+  if (!exists("bods")) {
+    data(bods, package = "pathview")
+  }
+  msg <- capture.output(pathview(...), type = "message")
+  msg <- grep("image file", msg, value = TRUE)
+  filename <- sapply(strsplit(msg, " "), function(x) x[length(x)])
+  if (length(filename) > 0 ) {
+    img <- readPNG(filename)
+    grid.raster(img)
+    nam <- str_split(filename, "[.]")
+    invisible(file.remove(filename))
+    invisible(file.remove(paste0(nam[[1]][1], ".xml")))
+    invisible(file.remove(paste0(nam[[1]][1], ".png")))
+  }
+  return()
+}
+
+# ----- Enrichment results ----
+#
+#' @title Get a particular enrichment result
+#' @description
+#' Called inside the getEnrichRes method
+#' 
+#' @param object a SE object or a MAE object (produced by Flomics).
+#' @return enrichment result.
+#' @noRd
+#' @keywords internal
+.getEnrichResInt <- function(
+    object, 
+    contrast, 
+    experiment = NULL, 
+    from ,
+    database,  
+    domain,
+    classObj
+){
+  
+  if (toupper(from) %in% c("DIFFEXP", "DIFFEXPANAL", "DIFFEXPENRICHANAL")){
+    from <- "DiffExpEnrichAnal"
+  }
+  if (toupper(from) %in% c("COEXP", "COEXPANAL", "COEXPENRICHANAL")) {
+    from <- "CoExpEnrichAnal"
+  }
+  
+  switch(classObj,
+         "SE" = {
+           if (is.null(contrast)) {
+             res_return <- object@metadata[[from]][[database]][["enrichResult"]]
+           } else {
+             if (isTagName(object, contrast)) contrast <- convertTagToContrast(object, contrast)
+             res_return <- object@metadata[[from]][[database]][["enrichResult"]][[contrast]]
+           }
+         },
+         "MAE" = {
+           if (is.null(experiment)) {
+             stop("Please indicate from which data you want to extract the enrichment results.")
+           }
+           
+           if (is.null(contrast)) {
+             res_return <- object[[experiment]]@metadata[[from]][[database]][["enrichResult"]]
+           } else {
+             if (isTagName(object, contrast)) contrast <- convertTagToContrast(object[[experiment]], contrast)
+             res_return <- object[[experiment]]@metadata[[from]][[database]][["enrichResult"]][[contrast]]
+           }
+         },
+         stop("Object is not a RflomicsMAE nor a RflomicsSE")
+  )
+}
+
+#
+#' @title Get a particular enrichment result
+#' @description
+#' Called inside the getEnrichSum method
+#' 
+#' @param object a SE object or a MAE object (produced by Flomics).
+#' @return enrichment result summary
+#' @noRd
+#' @keywords internal
+.getEnrichSumIntMAE <- function(object,
+                                experiment = NULL,
+                                from = "DiffExpEnrichAnal",
+                                database = "GO"){
+  if (is.null(experiment)) {
+    stop("Please indicate the experiment
+         from which you want to extract the enrichment results.")
+  }
+  return(object[[experiment]]@metadata[[from]][[database]][["summary"]])
+} 
+
+.getEnrichSumIntSE <- function(object,
+                               from = "DiffExpEnrichAnal",
+                               database = "GO"){
+  return(object@metadata[[from]][[database]][["summary"]])
+}
