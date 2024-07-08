f970d7a03bf60fdcf42a5e3abcfb8a4236fb8967
HULOT Audrey <audrey.hulot@inrae.fr>
2024-07-08 09:27:28 +0200

    smaller lines in example

--- inst/examples/filterFeatures.R
+++ inst/examples/filterFeatures.R
@@ -14,14 +14,10 @@ names(MAE) <- c("RNAtest", "metatest", "protetest")
 
 formulae <- generateModelFormulae( MAE) 
 MAE <- setModelFormula(MAE, formulae[[1]])
+contrastList <- Reduce(rbind, generateExpressionContrast(MAE)) 
 
-contrastList <- generateExpressionContrast(object = MAE) |> 
-    purrr::reduce(rbind) |>
-    dplyr::filter(contrast %in% c("(temperatureElevated_imbibitionDS - temperatureLow_imbibitionDS)",
-                                  "((temperatureLow_imbibitionEI - temperatureLow_imbibitionDS) + (temperatureMedium_imbibitionEI - temperatureMedium_imbibitionDS) + (temperatureElevated_imbibitionEI - temperatureElevated_imbibitionDS))/3",
-                                  "((temperatureElevated_imbibitionEI - temperatureLow_imbibitionEI) - (temperatureElevated_imbibitionDS - temperatureLow_imbibitionDS))" ))
 MAE <- MAE |>
-    setSelectedContrasts(contrastList) |>
+    setSelectedContrasts(contrastList[c(3,6,25)]) |>
     runTransformData(SE.name = "metatest", transformMethod = "log2") |>
     runNormalization(SE.name = "metatest", normMethod = "median")    |>
     runNormalization(SE.name = "protetest", normMethod = "median")   |>
@@ -35,7 +31,7 @@ selOpt = list("protetest" = c("cluster.1", "H3"), "metatest" = c("DE"))
 MAE1 <- filterFeatures(MAE, selOpt)
 MAE1
 
-selOpt2 = list("protetest" = c("cluster.2", "H2", "H1"), metatest = c("DE"))
+selOpt2 = list("protetest" = c("cluster.2", "H25"), metatest = c("DE"))
 MAE2 <- filterFeatures(MAE, selOpt2,
                        type = c("metatest" = "intersection",
                                 "protetest" = 'union'))
